6e86b35fc04f9e2cf6b69636410916db3115f907
yunpoyue <chinesehuazhou@gmail.com>
2017-11-01 20:27:04 +0800

    1

---
+++ count_word.py
@@ -0,0 +1,28 @@
+
+import re
+
+
+def count_word(filename):
+    f = open(filename, 'rb')
+    wdic = {}
+    for line in f:
+        line = line.lower().strip()
+        # \W 匹配任何不是Unicode字符的字符
+        words = re.split('\W+', line)
+        # words = re.findall('[a-zA-Z0-9]+', line)
+
+        for word in words:
+            if word in wdic:
+                wdic[word] += 1
+            else:
+                wdic[word] = 1
+    f.close()
+    # sort by frequency
+    # Returns a new sorted list with (key, value) pairs, dict cannot be sorted
+    wdic = sorted(wdic.items(), key=lambda x: x[1], reverse=True)
+    print(wdic)
+
+
+if __name__ == '__main__':
+    filename = 'testtext.txt'
+    count_word(filename)
